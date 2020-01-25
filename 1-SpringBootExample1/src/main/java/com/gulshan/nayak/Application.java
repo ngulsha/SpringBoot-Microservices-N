fc85fdbc28f8f6d5619860d827e6b5d687e56333
Gulshan Nayak <mr.gulshan.nayak@gmail.com>
2020-01-25 19:12:29 +0530

    Example1

---
+++ 1-SpringBootExample1/src/main/java/com/gulshan/nayak/Application.java
@@ -0,0 +1,15 @@
+package com.gulshan.nayak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+		System.out.println(applicationContext);
+	}
+
+}
